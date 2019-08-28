24be66ed65deb838ebb36af258eec615d695ee22
Aurel Pintea <winofame@gmail.com>
2019-08-28 15:10:07 +0100

    Minor updates and experiments.

---
+++ src/main/java/hello/ExcludedMavenCompilerPlugin.java
@@ -0,0 +1,9 @@
+//package hello;
+//
+//import static javax.xml.XMLConstants.XML_NS_PREFIX;
+//
+//public class MavenCompilerPlugin {
+//    public static void main(String[] args) {
+//        System.out.println("\n The XML namespace prefix is (drum sound): " + XML_NS_PREFIX);
+//    }
+//}
